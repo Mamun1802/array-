b55bc8b68eb58834f411454e2fd02b9bc613caee
MAMUN <almamun1802@gmail.com>
2024-09-07 20:01:27 +0600

    it prints the values

---
+++ print_values.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+int arr[]={65,6,2,70,12,45,78,23};
+ printf("Values of the array are = ");
+ for(int i=0;i<10;i++){
+    printf("%d ",arr[i]);
+
+}
+ return 0;
+
+}
